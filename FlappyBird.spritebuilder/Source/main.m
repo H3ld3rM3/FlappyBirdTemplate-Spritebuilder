1608b8be6f61eede490bc306abc6ab3bd693053b
H3ld3rM3 <h3ld3r.marqu3s@gmail.com>
2015-07-11 01:26:46 +0100

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/main.m
+++ FlappyBird.spritebuilder/Source/main.m
@@ -8,19 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CGPointObject : NSObject
-{
-    CGPoint _ratio;
-    CGPoint _offset;
-    CCNode *__unsafe_unretained _child; // weak ref
-}
-@property (nonatomic,readwrite) CGPoint ratio;
-@property (nonatomic,readwrite) CGPoint offset;
-@property (nonatomic,readwrite,unsafe_unretained) CCNode *child;
-+(id) pointWithCGPoint:(CGPoint)point offset:(CGPoint)offset;
--(id) initWithCGPoint:(CGPoint)point offset:(CGPoint)offset;
-@end
-
 
 int main(int argc, char *argv[]) {
     
